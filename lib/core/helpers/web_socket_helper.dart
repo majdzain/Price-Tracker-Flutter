037ab8584c721ffd40d251a85adeae7c6804c332
majdzain <zezoocvi.77@gmail.com>
2023-04-13 03:39:43 +0300

    add samples

--- lib/core/helpers/web_socket_helper.dart
+++ lib/core/helpers/web_socket_helper.dart
@@ -23,57 +23,21 @@ class WebSocketHelper {
       if (listen != null) {
         listen!(event);
       }
-      // _recipientCtrl.add(event);
     }, onError: (e) async {
-      // _recipientCtrl.addError(e);
-      // await Future.delayed(const Duration(seconds: 2));
       print('error');
-      // connect();
     }, onDone: () async {
       await Future.delayed(const Duration(seconds: 2));
       print('done');
-      // connect();
     });
-    // streamController = StreamController.broadcast(onCancel: () {});
-    // stream = channel.stream.asBroadcastStream(onCancel: (s) {
-    //   channel = WebSocketChannel.connect(
-    //     Uri.parse(APIConfig.channelServerUrl),
-    //   );
-    // }, onListen: (s) {
-    //   print(s);
-    // });
     isConnected = true;
   }
 
   void disconnect() {
-    // _sentCtrl.close();
     channel.sink.close();
   }
 
   void addData(String requestData) {
-    // if (!isConnected) {
-    //   connect();
-    // }
-    // connect();
     print(requestData);
     channel.sink.add(requestData);
   }
-
-  // Stream getStream() {
-  //   // if (!isConnected) {
-  //   //   connect();
-  //   // }
-  //   // channel.stream.listen((event) {
-  //   //   print(event);
-  //   //   streamController.add(event);
-  //   // });
-  //   // if (stream == null) {
-
-  //   // }
-  //   return _recipientCtrl.stream;
-  // }
-
-  // void cancelStream() {
-  //   channel.sink.close();
-  // }
 }
--- lib/features/tracker/data/datasources/remote/ticks_remote_datasource.dart
+++ lib/features/tracker/data/datasources/remote/ticks_remote_datasource.dart
@@ -12,9 +12,6 @@ class TicksRemoteDataSource {
   Future<void> getTickByTick(
       String symbol, Function(TickModel tickModel) retrieveTick) async {
     final String requestData = '{"ticks": "$symbol","subscribe": 1}';
-    retrieveTick(TickModel.fromJson(jsonDecode(
-            '{"ask": 215.4403,"bid": 215.4203,"epoch": 1675856816,"id": "417a56af-c9f1-cd25-654d-3c77e9025d18","pip_size": 4,"quote": 215.4303,"symbol": "R_50"}')
-        as Map<String, dynamic>));
     channelHelper.listen = (responseData) {
       Map<String, dynamic> data =
           jsonDecode(responseData as String) as Map<String, dynamic>;
